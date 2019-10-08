3eb41999952da4b1ba0fbb9f90f61161f18afb06
ubuntu <ubuntu@localhost.localdomain>
2019-10-08 16:56:06 +0800

    chore: upload config

--- git_uplaod.sh
+++ git_uplaod.sh
@@ -1,5 +1,5 @@
 cp themes/next/_config.yml theme_config/next/_config.yml
-hexo g && hexo d
+./node_modules/.bin/hexo g && ./node_modules/.bin/hexo d
 
 git add .
 git commit -m "chore: upload config"
