91e877bfa42e399f80ba69b13640ca9ba551615d
zayfen <zhangyunfeng0101@gmail.com>
2019-11-18 03:41:09 -0700

    chore: upload repo

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,3 +1,6 @@
+
+cp -r public public-backup
+
 ./node_modules/.bin/hexo clean
 ./node_modules/.bin/hexo g
 ./node_modules/.bin/hexo d
@@ -11,3 +14,11 @@ git push
 # update algolia
 export HEXO_ALGOLIA_INDEXING_KEY='a6accc6480b4a9053d9905fa81d61d9c'
 hexo algolia
+
+if [ -d "./public" ];
+then
+    rm -r public-backup
+else
+    mv public-back public
+fi
+
