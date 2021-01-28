8f063a877c39daff64ef6c76efdd976dd602fce4
zayfen <zhangyunfeng0101@gmail.com>
2021-01-28 17:35:10 +0800

    change script

--- update-next-theme.sh
+++ update-next-theme.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
-rm -r themes/next
+# rm -r themes/next
+# git clone https://github.com/theme-next/hexo-theme-next themes/next --depth=1
 
-git clone https://github.com/theme-next/hexo-theme-next themes/next --depth=1
+git submodule foreach --recursize git pull origin master
 
 
